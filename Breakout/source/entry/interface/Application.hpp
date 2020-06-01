acb9e012082281d6fdc64a651c025987b8bd1fd7
Igor Obradovic <igor.obrado@gmail.com>
2020-06-01 13:17:01 +0200

    Add gitignore file and basic mock of application

---
+++ Breakout/source/entry/interface/Application.hpp
@@ -0,0 +1,14 @@
+#ifndef BREAKOUT_APPLICATION_HPP
+#define BREAKOUT_APPLICATION_HPP
+
+#include <iostream>
+
+class Aplication
+{
+public:
+    virtual int run() = 0;
+    virtual ~Aplication() = default;
+};
+
+
+#endif //BREAKOUT_APPLICATION_HPP
