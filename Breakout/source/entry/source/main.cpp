6dfcc23a63dd436e39fb21594f6e9689533dee64
Igor Obradovic <igor.obrado@gmail.com>
2020-06-04 19:04:38 +0200

    Differentiate between windows and unix

--- Breakout/source/entry/source/main.cpp
+++ Breakout/source/entry/source/main.cpp
@@ -1,9 +1,13 @@
 #include "sandbox/Breakout.hpp"
-///#include "State.hpp"
+
+#ifndef WIN_32
 int main()
+#else  //
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
+#endif
 {
-    std::unique_ptr<Application> breakout = std::make_unique<game::Breakout>(
-            ScreenDimensions{ 800, 600  },
+    std::unique_ptr<Application> breakout = std::make_unique<game::Breakout>(  //
+            ScreenDimensions{ 800, 600 },
             "Breakout");
     return breakout->run();
-}
+}  //
