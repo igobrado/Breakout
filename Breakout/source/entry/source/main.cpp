d9783baab7e2b010e0737f9e73495aabf5c43a50
Igor Obradovic <igor.obrado@gmail.com>
2020-06-08 17:55:31 +0200

    Add debug define in cmake and use it to track allocations

--- Breakout/source/entry/source/main.cpp
+++ Breakout/source/entry/source/main.cpp
@@ -1,12 +1,40 @@
 #include "sandbox/Breakout.hpp"
 
+#ifdef DEBUG
+#include <fstream>
+static long allocations = 0;
+
+void* operator new(size_t size)
+{
+    ++allocations;
+
+    void * p = malloc(size);
+    return p;
+}
+void operator delete(void* p)
+{
+    --allocations;
+	free(p);
+}
+#endif
+
 #ifndef WIN_32
 int main()
 #else  //
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
 #endif
 {
-    std::unique_ptr<Application> breakout = std::make_unique<game::Breakout>(  //
-            "Breakout");
-    return breakout->run();
+    int rv = 0;
+
+    {///< only to track are all objects clean up after game finishes
+     ///< TODO: DELETE BEFORE RELEASE
+	    std::unique_ptr<Application> breakout = std::make_unique<game::Breakout>(  //
+		    "Breakout");
+        rv = breakout->run();
+    }
+#ifdef DEBUG
+    std::ofstream file("Allocations.txt");
+    file << allocations;
+#endif
+    return rv;
 }  //
