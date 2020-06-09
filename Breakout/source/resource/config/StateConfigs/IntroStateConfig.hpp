63ad7828a55736e91246689974779877f1168f7d
Igor Obradovic <igor.obrado@gmail.com>
2020-06-09 19:56:52 +0200

    Add docs for Intro state config class

--- Breakout/source/resource/config/StateConfigs/IntroStateConfig.hpp
+++ Breakout/source/resource/config/StateConfigs/IntroStateConfig.hpp
@@ -6,6 +6,12 @@
 #include "Widget.hpp"
 #include "private/StateDefinitions.hpp"
 
+/**
+ * @brief Represents configuration for intro state.
+ *
+ * If there is no config in provided xmlPath level state won't terminate.
+ * It shall continue working, but without features like, textboxes (if any).
+ */
 class IntroStateConfig : public Config
 {
 public:
